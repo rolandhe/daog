7128166d6ba3efaeec8076084bd5cd8bbe12ffa3
hexiufeng <rolandhe@qq.com>
2023-01-27 11:58:20 +0800

    add batch process data for large query

--- query.go
+++ query.go
@@ -3,7 +3,12 @@
 // Copyright 2023 The daog Authors. All rights reserved.
 package daog
 
-import "time"
+import (
+	"errors"
+	"time"
+)
+
+var invalidPageSizeError = errors.New("page size must be greater than 0")
 
 func GetById[T any](id int64, meta *TableMeta[T], tc *TransContext) (*T, error) {
 	m := NewMatcher()
@@ -54,6 +59,62 @@ func QueryListMatcher[T any](m Matcher, meta *TableMeta[T], tc *TransContext) ([
 	return inses, nil
 }
 
+type PageHandler[T any] func(page []*T) error
+
+// QueryListMatcherPageHandle 读取数据并且分页处理数据，当读取的数据量巨大时非常有用，如果数据都读入内存，容易打爆内存，分批量处理就非常有用
+// pageSize 每批处理数据的最大容量，必须大于0，但不要设置太大，当设置为1时，退化成每条处理
+// handler 用于处理每批数据的函数
+func QueryListMatcherPageHandle[T any](m Matcher, meta *TableMeta[T],pageSize int,handler PageHandler[T],tc *TransContext) error {
+	var err error
+	if pageSize <= 0 {
+		return invalidPageSizeError
+	}
+	err = tc.check()
+	if err != nil {
+		return err
+	}
+
+	sql, args := selectQuery(meta, tc.ctx, m)
+	if tc.LogSQL {
+		sqlMd5 := traceLogSQLBefore(tc.ctx, sql, args)
+		defer traceLogSQLAfter(tc.ctx, sqlMd5, time.Now().UnixMilli())
+	}
+	rows, err := tc.conn.QueryContext(tc.ctx, sql, args...)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	page := make([]*T,pageSize)
+	index := 0
+
+	for rows.Next() {
+		ins, scanFields := buildInsInfoOfRow(meta)
+		if err = rows.Scan(scanFields...); err != nil {
+			return  err
+		}
+		page[index] = ins
+		index++
+		if index == pageSize {
+			if err = handler(page);err != nil{
+				return err
+			}
+			index = 0
+		}
+	}
+
+	if index == 0 {
+		return nil
+	}
+
+	page = page[:index]
+	if err = handler(page);err != nil{
+		return err
+	}
+
+	return nil
+}
+
 func QueryOneMatcher[T any](m Matcher, meta *TableMeta[T], tc *TransContext) (*T, error) {
 	var err error
 	err = tc.check()
@@ -81,13 +142,12 @@ func QueryOneMatcher[T any](m Matcher, meta *TableMeta[T], tc *TransContext) (*T
 	return ins, nil
 }
 
-type RowFieldPointMapper[T any] interface {
-	ExtractScanFieldPoints(ins *T) []any
-}
+
+type ExtractScanFieldPoints[T any] func(ins *T) []any
 
 // QueryRawSQL 执行原生select sql语句,返回行数据数组，行数据使用T struct描述
 // mapper, 它T的各个field指针提取出来并按照顺序生成一个slice，用于Row.Scan方法，把sql字段映射到T对象的各个Field上
-func QueryRawSQL[T any](tc *TransContext, mapper RowFieldPointMapper[T], sql string, args ...any) ([]*T, error) {
+func QueryRawSQL[T any](tc *TransContext, extract ExtractScanFieldPoints[T], sql string, args ...any) ([]*T, error) {
 	var err error
 	err = tc.check()
 	if err != nil {
@@ -106,7 +166,7 @@ func QueryRawSQL[T any](tc *TransContext, mapper RowFieldPointMapper[T], sql str
 	var inses []*T
 	for rows.Next() {
 		ins := new(T)
-		scanFields := mapper.ExtractScanFieldPoints(ins)
+		scanFields := extract(ins)
 		if err = rows.Scan(scanFields...); err != nil {
 			return nil, err
 		}
