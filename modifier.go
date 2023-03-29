6267325259a34e1d151bc1c84c1291c3a753bd5d
hexiufeng <hexiufeng@reta-inc.com>
2023-03-29 15:25:50 +0800

    增加update a=a+?

--- modifier.go
+++ modifier.go
@@ -8,9 +8,15 @@ import (
 	"strings"
 )
 
+const (
+	selfAdd   = 1
+	selfMinus = 2
+)
+
 type pair struct {
 	column string
 	value  any
+	self   int
 }
 
 // NewModifier 创建 Modifier 对象
@@ -23,6 +29,8 @@ func NewModifier() Modifier {
 type Modifier interface {
 	// Add 增加一个字段的修改，比如 id = 100
 	Add(column string, value any) Modifier
+	SelfAdd(column string, value any) Modifier
+	SelfMinus(column string, value any) Modifier
 	toSQL(tableName string) (string, []any)
 }
 
@@ -31,7 +39,17 @@ type internalModifier struct {
 }
 
 func (m *internalModifier) Add(column string, value any) Modifier {
-	m.modifies = append(m.modifies, &pair{column, value})
+	m.modifies = append(m.modifies, &pair{column, value, 0})
+	return m
+}
+
+func (m *internalModifier) SelfAdd(column string, value any) Modifier {
+	m.modifies = append(m.modifies, &pair{column, value, 1})
+	return m
+}
+
+func (m *internalModifier) SelfMinus(column string, value any) Modifier {
+	m.modifies = append(m.modifies, &pair{column, value, 2})
 	return m
 }
 
@@ -43,7 +61,13 @@ func (m *internalModifier) toSQL(tableName string) (string, []any) {
 	modStmt := make([]string, l)
 	args := make([]any, l)
 	for i, p := range m.modifies {
-		modStmt[i] = p.column + "=?"
+		if p.self == selfAdd {
+			modStmt[i] = p.column + "=" + p.column + "+?"
+		} else if p.self == selfMinus {
+			modStmt[i] = p.column + "=" + p.column + "-?"
+		} else {
+			modStmt[i] = p.column + "=?"
+		}
 		args[i] = p.value
 	}
 	return "update " + tableName + " set " + strings.Join(modStmt, ","), args
